2a4ea125f145bd530c64750b84cd3bd63cf1d94e
ROKUMATE <rohitkumawat0110@gmail.com>
2024-05-26 18:27:42 +0530

    forgot to push user.routes

--- src/routers/user.routes.js
+++ src/routers/user.routes.js
@@ -12,7 +12,7 @@ router.route("/register").post(
             maxCount: 1,
         },
         {
-            name: "cover-image",
+            name: "coverImage",
             maxCount: 1,
         },
     ]),
