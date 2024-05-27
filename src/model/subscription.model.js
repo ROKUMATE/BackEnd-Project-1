7c7e9f58cf71b79c2b8cd61359fa40cb1f88ae9d
ROKUMATE <rohitkumawat0110@gmail.com>
2024-05-28 02:21:07 +0530

    Subscription Model Added

--- src/model/subscription.model.js
+++ src/model/subscription.model.js
@@ -3,8 +3,15 @@ import mongoose from "mongoose";
 
 const subscrptionSchema = mongoose.Schema(
     {
+        //
         subscriber: {
             type: mongoose.Types.ObjectId,
+            // One Who is Subscribing
+            ref: "User",
+        },
+        channel: {
+            type: mongoose.Types.ObjectId,
+            // One to Whome the Subscriber is Subscribing
             ref: "User",
         },
     },
